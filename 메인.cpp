3fd996d05599fca5ec36138dabef277710c75c94
qortjddbs(notebook) <beak980513@naver.com>
2026-03-31 17:19:44 +0900

    3월 31일 수업 끝

--- 메인.cpp
+++ 메인.cpp
@@ -1,48 +1,33 @@
 //-------------------------------------------------------------------------------------------------------
-// 2026년 1학기 STL 월56 화78		3월 30일																					(4주 2일)
+// 2026년 1학기 STL 월56 화78		3월 31일																					(5주 1일)
 //------------------------------------------------------------------------------------------------------- 
-// callable type -> 실습 -> STL
+// 호출가능 타입 - callable types
+// 1. 함수
+//      - 함수포인터도 호출가능하다
+// 2. lambda
+// 3. () 연산자를 구현한 클래스 - 객체를 functor(함수객체)라 함
+// 4. 멤버함수와 멤버함수 포인터
+// 
+// ---> function 클래스로 일관된 사용가능
 //-------------------------------------------------------------------------------------------------------
 #include <iostream>
-#include <random>
-#include <print>
-#include <array>
-#include <ranges>
 #include "save.h"
-
-// [문제] 랜덤값을 갖는 int 1000만개를 메모리에 저장하라.
-// qsort를 이용하여 오름차순 정렬하라.
-// 정렬결과를 앞에서부터 1000개만 화면에 출력하라.
-
-std::default_random_engine dre;
-std::uniform_int_distribution uid{ 0, 999'9999 };
-
-std::array<int, 1000'0000> a;
-
-int 오름차순(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-}
+class Sakura {
+public:
+    void operator()() {
+        std::cout << "안녕 난 람다야 반가워" << std::endl;
+    };
+};
 
 // ----------
 int main()
 // ----------
 {
+
+    Sakura a;
+    a();
+
+    std::cout << typeid(a).name() << std::endl;
+
     save("메인.cpp");
-
-    for (int& num : a) {
-        num = uid(dre);
-   }
-
-    // 시간측정시작
-    qsort(a.data(), a.size(), sizeof(std::array<int, 1000'0000>::value_type), 오름차순);
-    // 측정 끝
-	
-    std::cout << "정렬 후 출력" << std::endl;
-
-    for (int num : a | std::views::reverse
-                                | std::views::take(1000)) {      // | : 버티컬 바 (짝대기) -> 필터 역할 (데이터를 원하는 대로 뽑아줄 수 있음!!)
-        std::print("{:8}", num);
-    }
-
-    std::cout << std::endl;
 }
